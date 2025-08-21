e9ecca742d05b6a1276edfdc3495dfe1b4b422c8
anacatarinapalmeida <uc2022222845@student.uc.pt>
2025-08-21 17:40:38 +0100

    comentários no js

--- js/slider.js
+++ js/slider.js
@@ -3,6 +3,12 @@ const conteudo = document.getElementById("conteudoProjeto");
 
 const imagens = document.querySelectorAll(".slider img");
 
+
+// recebe imagem como parametro
+// vai buscar data-titulo e data-conteudo da imagem e guarda templates (#titulo-ccdm...)
+// limpa html
+// copia o conteudo dos templates para #tituloProjeto e #conteudoProjeto
+
 function atualizar(img) {
     // atualizar titulo
     const tituloTemplate = document.querySelector(img.dataset.titulo);
@@ -15,18 +21,26 @@ function atualizar(img) {
     conteudo.appendChild(conteudoTemplate.content.cloneNode(true));
 }
 
+
+// entry.isIntersecting - imagem entrou no ecra
+// entry.intersectionRatio > 0.8 - pelo menos 80% da imagem esta visivel (mais como seguranca, nao e obrigatorio)
+// threshold: [0.8] - define o momento 
+// se tudo ok entao atualiza
+
 const centrado = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-        if (entry.isIntersecting && entry.intersectionRatio > 0.5) {
+        if (entry.isIntersecting && entry.intersectionRatio > 0.8) {
             atualizar(entry.target);
         }
     });
 
 }, {
     root: document.querySelector(".slider"),
-    threshold: [0.5]
+    threshold: [0.8]
 });
 
+
+//observa todas as imagens 
 imagens.forEach(img => observer.observe(img));
 
 // mostrar automaticamente o 1 projeto quando carrega pag
